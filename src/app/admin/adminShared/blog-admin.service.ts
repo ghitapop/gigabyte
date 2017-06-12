e70f48a23da2c46316f017912565c4faa2d0dc1e
ghitapop <ghita.pop1608@gmail.com>
2017-06-12 19:58:29 +0200

    fixing again the firebase issue

--- src/app/admin/adminShared/blog-admin.service.ts
+++ src/app/admin/adminShared/blog-admin.service.ts
@@ -9,23 +9,43 @@ export class BlogAdminService {
 
   createPost(post: Blog, callback: any) {
     let storageRef = firebase.storage().ref();
-    let uploadTask = storageRef.child('image/' + post.imgTitle).putString(post.img, 'base64');
+    let imgKey = 'image/' + post.imgTitle;
+    let uploadTask = storageRef.child(imgKey).putString(post.img, 'base64');
 
-    uploadTask.on('state_changed', function(snapshot){
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      //console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          //console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          //console.log('Upload is running');
-          break;
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      function(snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
         }
       }, function(error) {
-        console.log(error.message);
+
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.name) {
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect error.serverResponse
+          break;
+      }
     }, function() {
-      let url = uploadTask.snapshot.downloadURL[0];
+      // Upload completed successfully, now we can get the download URL      
+      let url = uploadTask.snapshot.downloadURL;
       let dbRef = firebase.database().ref('blogPosts/');
       let newPost = dbRef.push();
       newPost.set({
@@ -35,7 +55,7 @@ export class BlogAdminService {
         img: url,
         id: newPost.key
       }); 
-      callback();   
+      callback(); 
     });
   }
 }
