9348c832688b244c740d71596fcce0f200208c3e
ghitapop <ghita.pop1608@gmail.com>
2017-06-12 19:45:36 +0200

    fixed path issue when saving blog images

--- src/app/admin/adminShared/blog-admin.service.ts
+++ src/app/admin/adminShared/blog-admin.service.ts
@@ -9,23 +9,23 @@ export class BlogAdminService {
 
   createPost(post: Blog, callback: any) {
     let storageRef = firebase.storage().ref();
-    let uploadTask = storageRef.child('image/${post.imgTitle}').putString(post.img, 'base64');
+    let uploadTask = storageRef.child('image/' + post.imgTitle).putString(post.img, 'base64');
 
     uploadTask.on('state_changed', function(snapshot){
       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
+      //console.log('Upload is ' + progress + '% done');
       switch (snapshot.state) {
         case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
+          //console.log('Upload is paused');
           break;
         case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
+          //console.log('Upload is running');
           break;
         }
       }, function(error) {
         console.log(error.message);
     }, function() {
-      let url = uploadTask.snapshot.downloadURL;
+      let url = uploadTask.snapshot.downloadURL[0];
       let dbRef = firebase.database().ref('blogPosts/');
       let newPost = dbRef.push();
       newPost.set({
